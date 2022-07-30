88a6f58c4e48fce81a3421e379939ae19d3fc9fc
Sam Clark <samclark2399@gmail.com>
2022-07-30 19:02:10 -0400

    initial commit

---
+++ src/main/java/com/labassistantapi/LabAssistantApiApplication.java
@@ -0,0 +1,13 @@
+package com.labassistantapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabAssistantApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabAssistantApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/labassistantapi/LabAssistantApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labassistantapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabAssistantApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
