4644d6bb61ea44d88e9b77316a17b39d9d4e625b
Sam Clark <samclark2399@gmail.com>
2022-08-03 13:25:08 -0400

    created Calculation model

---
+++ src/main/java/com/labassistantapi/model/Calculation.java
@@ -0,0 +1,62 @@
+package com.labassistantapi.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table(name = "calculation")
+public class Calculation implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String title;
+
+    public Calculation() {
+    }
+
+    public Calculation(Integer id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Calculation that = (Calculation) o;
+        return Objects.equals(id, that.id) && Objects.equals(title, that.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title);
+    }
+
+    @Override
+    public String toString() {
+        return "Calculation{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                '}';
+    }
+}
